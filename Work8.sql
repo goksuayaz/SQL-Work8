ea513cdce6e078e6ec76a7b104e0e064d237f7e4
Göksu Ayaz <goksuayaz79@gmail.com>
2024-03-25 18:30:56 +0300

    Work8

---
+++ Work8.sql
@@ -0,0 +1,24 @@
+--CREATE TABLE employee (id SERIAL PRIMARY KEY, name VARCHAR(50), birthday DATE, email VARCHAR(100));
+
+--UPDATE employee SET name = 'Brunhilda Cooper' WHERE name = 'Brunhilda Coop';
+
+--UPDATE employee SET name = 'Olly Melody' WHERE NAME = 'Olly Melledy';
+
+--UPDATE employee SET email = 'bcooper@multiply.com' WHERE email = 'bcoop0@multiply.com';
+
+--UPDATE employee SET email = 'omelody5@businessweek.com' WHERE email = 'omelledy5@businessweek.com';
+
+--UPDATE employee SET birthday = '1990-12-11' WHERE birthday = '1990-11-29';
+
+--SELECT * FROM employee;
+
+--DELETE FROM employee WHERE name = 'Garold Elsip';
+--DELETE FROM employee WHERE birthday = '1998-06-13';
+--DELETE FROM employee WHERE email = 'mpesik3@vistaprint.com';
+--DELETE FROM employee WHERE id = 25;
+--DELETE FROM employee WHERE id = 42;
+
+--SELECT * FROM employee;
+
+
+
